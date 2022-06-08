86d24768b70bb529ca056491da8e615878c085d3
Elisa.J <elisa.joudal@gmail.com>
2022-06-09 00:01:11 +0200

    MAJ 8 juin

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -1,19 +1,31 @@
-function grandir(){
+
+//gestion du bouton menu
+var menu = document.getElementById("menu");
+var burger = document.getElementById("iconBurger");
+document.getElementById("burger").addEventListener("click", function () {
+    menu.classList.toggle("open");
+    burger.classList.toggle("fa-xmark");
+    burger.classList.toggle("fa-bars");
+});
+
+
+// function grandir(){
 
     
-    var largeur = 5;
-    var id = setInterval(progression, 50);
-    function progression(){
-        if (largeur < 80){
-            largeur ++;
-            console.log(largeur);
-        }
-        else {
-            clearInterval(id);
-        }
-    }
-}
-var elem = document.querySelector('.progress1');
-elem.addEventListener('click', function() {
-    grandir();
-})
+//     var largeur = 5;
+//     var id = setInterval(progression, 50);
+//     function progression(){
+//         if (largeur < 80){
+//             largeur ++;
+//             console.log(largeur);
+//         }
+//         else {
+//             clearInterval(id);
+//         }
+//     }
+// }
+// var elem = document.querySelector('.progress1');
+// elem.addEventListener('click', function() {
+//     grandir();
+// })
+
